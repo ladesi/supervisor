8f982335f082c37ffe64b0e5be889ccf82b7b388
devmuu <m.andrade@alu.ufc.br>
2024-06-21 13:51:44 -0300

    changing lang variable

--- app/lab_control/settings.py
+++ app/lab_control/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-BR'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Fortaleza'
 
