d6370d1ff4fcfa77a4454897220a8e746b1e4a44
devmuu <m.andrade@alu.ufc.br>
2024-06-28 17:02:01 -0300

    adjust base url in update table script

--- app/static/js/update.js
+++ app/static/js/update.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
     // var tab_url = $('#analyser_data').load($('#analyser_data').attr("data-url"));
+    var base_url = window.location.origin;
+
     $.ajaxSetup({
         cache: false
     });
     //event.preventDefault();
     setInterval(function() {
-        $('#analyser_table').load("http://127.0.0.1:8000/medidor/table");
+        $('#analyser_table').load(base_url + "/medidor/table");
     }, 2000);
 });
