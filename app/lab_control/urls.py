941a6e36ae6b7a7e9ed4a930214f972f1a767e79
devmuu <m.andrade@alu.ufc.br>
2025-02-19 23:29:31 -0300

    update settings to allow admin login and logout

--- app/lab_control/urls.py
+++ app/lab_control/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('sobre/', views.about, name='home-about'),
     path('medidor/', include('analyser.urls')),
     path('admin/', admin.site.urls),
+    path('logout/', views.UserLogoutView.as_view(http_method_names = ['get', 'post', 'options']), name="logout"),
 ]
--- app/lab_control/views.py
+++ app/lab_control/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from analyser.views import context
+from django.contrib.auth import logout
+from django.contrib.auth.views import LogoutView
 
 
 def home(request):
     return render(request, 'home.html', context)
 
+
 def about(request):
     return render(request, 'about.html', context)
+
+
+class UserLogoutView(LogoutView):
+    def get(self, request):
+        logout(request)
+        return redirect('home')
